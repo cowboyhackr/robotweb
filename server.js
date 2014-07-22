42ae2d5c661221402b2d7b8cf21c874f5f284911
Gandalf The White <gandalfthewhite@gandalfs-mini.attlocal.net>
2014-07-22 15:47:32 -0500

    amqp add on

--- server.js
+++ server.js
@@ -2,21 +2,51 @@
 
 var express = require('express');
 var app = express();
+var moment = require('moment');
+var config = require('./config')
 
-//configuration
 
-// app.configure(function(){
-// 	app.use(express.static(__dirname + '/public'));
-// 	app.use(express.logger('dev'));
-// 	app.use(express.bodyParser());
-// });
 app.get('/', function (req, res) {
   res.send('Hello from the robots')
 })
 
 app.get('/api/command', function(req,res){
 	//send command to rabbitmq
+
+	var command = req.query.command;
+	console.log(req.query.command);
+	var q = 'tilt2';
+	var url = config.amqp.url;
+
+	console.log("connecting to... " + url);
+	var open = require('amqplib').connect(url);
+
+	open.then(function(conn) {
+	  var ok = conn.createConfirmChannel();
+	  ok = ok.then(function(ch) {
+	    ch.assertQueue(q);
+	    ch.on
+
+		console.log('debug 1:  ' + moment().format());
+
+				console.log('debug 2:  ' + moment().format());
+				ch.sendToQueue(q, new Buffer(command), {type : 'tilt'}, function(err, ok) {
+						console.log('debug 3:  ' + moment().format());
+		            if (err !== null){
+		               console.log('Message nacked!:  ' + moment().format());
+		            }else{
+		               console.log('Message acked:  ' + moment().format());
+		            }
+				});
+
+		});
+			console.log('debug 5:  ' + moment().format());
+	  return ok;
+	}).then(null, console.warn);
+
+	res.send('waiting for command.')
 });
+
 
 //listen (start app with node server.js)
 app.listen(8080);
