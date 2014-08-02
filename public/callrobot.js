a118a92dcfa024e9fe23e84058c95d17144c2127
root <john.m.mckenna@live.com>
2014-08-02 11:43:34 -0500

    update address to cloud

--- public/callrobot.js
+++ public/callrobot.js
@@ -14,17 +14,19 @@ function SendRobotCommand($scope, $http) {
         });
 }*/
 
-
+//http://fierce-beach-8075.herokuapp.com/api/command?command=test
 function SendRobotCommand2($scope, $http) {
-  $http({method: 'GET', url: 'http://localhost:3000/api/command?command=turn%20100%201'}).
+  $http({method: 'GET', url: 'http://fierce-beach-8075.herokuapp.com/api/command?command=turn%20100%201'}).
     success(function(data, status, headers, config) {
       // this callback will be called asynchronously
       // when the response is available
+      $scope.greeting = data;
       console.log('success');
     }).
     error(function(data, status, headers, config) {
       // called asynchronously if an error occurs
       // or server returns response with an error status.
+      $scope.greeting = data;
       console.log('fail');
     });
 }
