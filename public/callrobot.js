e13d64e5271baf55d0094682435dbbb81842d06e
root <john.m.mckenna@live.com>
2014-08-02 09:36:53 -0500

    working with robot backend now

--- public/callrobot.js
+++ public/callrobot.js
@@ -1,4 +1,4 @@
-function Hello($scope, $http) {
+/*function Hello($scope, $http) {
     $http.get('http://rest-service.guides.spring.io/greeting').
         success(function(data) {
             $scope.greeting = data;
@@ -6,10 +6,40 @@ function Hello($scope, $http) {
 
 
 function SendRobotCommand($scope, $http) {
-    $http.get('http://localhost:3000/api/command?command=what').
+    $http.get('http://localhost:3000/api/command?command=turn%20100%201').
         success(function(data) {
             $scope.greeting = data;
             console.log('in success');
             console.log(data);
         });
+}*/
+
+
+function SendRobotCommand2($scope, $http) {
+  $http({method: 'GET', url: 'http://localhost:3000/api/command?command=turn%20100%201'}).
+    success(function(data, status, headers, config) {
+      // this callback will be called asynchronously
+      // when the response is available
+      console.log('success');
+    }).
+    error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      console.log('fail');
+    });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
